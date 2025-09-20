a7ba743a2902e2e2f805f48093c9db97aa2f6a3c
Brito Jimenez <m.brito36@unapec.edu.do>
2025-09-19 17:08:32 -0700

    Primeras 4 tablas del sistema

---
+++ BDNominas/Tables/Departamentos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Departamentos]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(128) NOT NULL, 
+    [UbicacionFisica] NVARCHAR(128) NOT NULL, 
+    [idResponsableArea] INT NOT NULL
+)
---
+++ BDNominas/Tables/Puestos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Puestos]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] VARCHAR(64) NOT NULL, 
+    [NivelDeRiesgo] SMALLINT NOT NULL, 
+    [MinimoSalario] MONEY NOT NULL DEFAULT 14161.00, 
+    [MaximoSalario] INT NOT NULL
+)
