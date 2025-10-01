a0ee504af3bd6d5d3165c6f1d5bc5bfe7765d850
Brito Jimenez <m.brito36@unapec.edu.do>
2025-10-01 18:19:31 -0400

    Proyecto de BD blueprint (base)

---
+++ SistemaDeNominas.BD.BluePrint/squema/Tablas/Empleados.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Empleados]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Cedula] NCHAR(11) NOT NULL, 
+    [Nombre] VARCHAR(64) NOT NULL, 
+    [SalarioMensual] NUMERIC NOT NULL, 
+    [idDepartamento] INT NOT NULL, 
+    [idPuesto] INT NOT NULL, 
+    [idNomina] INT NOT NULL 
+)
---
+++ SistemaDeNominas.BD.BluePrint/squema/Tablas/TiposDeIngreso.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TiposDeIngreso]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(64) NOT NULL, 
+    [idEmpleado] INT NOT NULL, 
+    [Estado] NVARCHAR(64) NOT NULL
+)
